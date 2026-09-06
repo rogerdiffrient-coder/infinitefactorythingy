cf32940de8fb2b8095d9ac5c2df2485c61d82b0d
How2FixBug <rogerdiffrient@gmail.com>
2026-09-06 11:43:37 -0600

    Load daylight fill rebalance

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import { WORLD_CONFIG, PLAYER_CONFIG, RENDER_CONFIG } from './config.js?v=visibility-shadows-8';
+import { WORLD_CONFIG, PLAYER_CONFIG, RENDER_CONFIG } from './config.js?v=daylight-fill-10';
 import { InputState } from './input/input.js';
 import { getBlockDefinition, getBlockMaterial } from './world/blockRegistry.js?v=terrain-restore-9';
 import { VoxelWorld } from './world/world.js?v=terrain-restore-9';
 import { WorldManager } from './world/worldManager.js';
 import { BlockInteraction } from './world/blockInteraction.js?v=speedbridge-2';
-import { DayNightCycle } from './world/dayNightCycle.js?v=visibility-shadows-8';
+import { DayNightCycle } from './world/dayNightCycle.js?v=daylight-fill-10';
 import { PlayerController } from './player/playerController.js';
 import { HealthSystem } from './player/healthSystem.js';
 import { TitleScreen } from './ui/titleScreen.js';
