ccac76e2cc46b3052339592a567ada8ad0a1d336
How2FixBug <rogerdiffrient@gmail.com>
2026-09-06 17:14:30 -0600

    Load corrected voxel normals renderer

--- src/world/world.js
+++ src/world/world.js
@@ -1,6 +1,6 @@
 import { WORLD_CONFIG } from '../config.js?v=terrain-restore-9';
 import { BLOCKS, getBlockDefinition } from './blockRegistry.js?v=terrain-restore-9';
-import { Chunk } from './chunk.js?v=terrain-restore-9';
+import { Chunk } from './chunk.js?v=normals-13';
 import { TerrainGenerator } from './terrainGenerator.js';
 
 const SX = WORLD_CONFIG.CHUNK_SIZE_X;
